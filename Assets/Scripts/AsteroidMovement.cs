c1b2331eaca1adb51986b0481e570a72879668d7
Henery Linck Rosenhaim <henery@trinca.com>
2026-06-02 10:55:25 -0300

    Tuning asteroid spawn/destruction and speed.

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -14,6 +14,7 @@ public class AsteroidMovement : MonoBehaviour
 
     [Header("General Settings")]
     [SerializeField] private float asteroidDuration = 15f;
+    [SerializeField] private float viewportMargin = 0.1f;
 
     private float _linearSpeed;
     private float _angularSpeed;
@@ -31,12 +32,21 @@ public class AsteroidMovement : MonoBehaviour
         _angularSpeed = Random.Range(minimumAngularSpeed, maximumAngularSpeed);
 
         //Randomize the direction of the asteroid within a margin of the player's ship direction
-        float angle = Random.Range(_player.transform.eulerAngles.x + minMargin, _player.transform.eulerAngles.y + maxMargin) * Mathf.Deg2Rad;
-        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        Vector2 direction = CalculateDirection();
 
         // Apply the calculated velocity and rotation to the asteroid's Rigidbody2D
         _rigidBody.linearVelocity = direction * _linearSpeed;
         _rigidBody.angularVelocity = _angularSpeed;
+    }
+
+    private Vector2 CalculateDirection()
+    {
+        Vector2 direction = (_player.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        angle += Random.Range(minMargin, maxMargin);
+        float radians = angle * Mathf.Deg2Rad;
+        Vector2 finalDirection = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)).normalized;
+        return finalDirection;
     }
 
     private void Update()
@@ -49,10 +59,12 @@ public class AsteroidMovement : MonoBehaviour
 
     private bool IsOutsideCameraBounds()
     {
-        // Convert the object's position to Viewport coordinates
         Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
 
         // Check if the central pivot point is outside the screen limits
-        return (viewportPos.x < 0 || viewportPos.x > 1 || viewportPos.y < 0 || viewportPos.y > 1);
+        return (viewportPos.x < -viewportMargin    ||
+                viewportPos.x > 1 + viewportMargin ||
+                viewportPos.y < -viewportMargin    ||
+                viewportPos.y > 1 + viewportMargin);
     }
 }
